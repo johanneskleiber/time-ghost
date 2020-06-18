bf3dcc4fafda4e455d2862722ac02b33238ff29c
Johannes <johannes.kleiber@gmail.com>
2020-06-18 22:36:14 +0200

    Main (Timer) view first databind prototype

---
+++ time-ghost/time-ghost.Core/Models/Timer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace time_ghost.Core.Models
+{
+    public class Timer
+    {
+        public string Description { get; set; }
+    }
+}
--- time-ghost/time-ghost/ViewModels/MainViewModel.cs
+++ time-ghost/time-ghost/ViewModels/MainViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using Prism.Windows.Mvvm;
+using time_ghost.Core.Models;
 
 namespace time_ghost.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public List<Timer> MondayTimers { get; private set; }
+
         public MainViewModel()
         {
+            this.MondayTimers = new List<Timer>
+            {
+                new Timer() { Description = "Hello Timer 1" },
+                new Timer() { Description = "Hello Timer 2" },
+                new Timer() { Description = "Hello Timer 3" },
+                new Timer() { Description = "Hello Timer 4" }
+            };
         }
     }
 }
